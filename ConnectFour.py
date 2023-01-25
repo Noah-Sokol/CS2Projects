99c47295fb95db12af7972393c513046d3bcb836
Noah-Sokol <nsokol25@gcds.net>
2023-01-24 23:02:52 -0500

    Update ConnectFour.py

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,1 +1,35 @@
+class user_input():
+    def __init__(self,var,board) -> None:
+        self.var = var
+        self.board = board
+    def works(self) -> int:
+        for i in range(len(self.board[self.var])):
+            if self.board[self.var][i] == 'o': return i
+        return False
 
+
+
+    
+
+
+
+    
+
+
+
+def get_user_input():
+    row = int(input('which row would you like to chose'))
+    board = [['x','x','x','x','o'],['o','o','o','o','o']]
+    x=user_input(row,board)
+    collumn = x.works()
+    print(board[row][collumn])
+
+
+def main():
+    get_user_input()
+
+
+main()
+
+#check for a win
+#loop until win
